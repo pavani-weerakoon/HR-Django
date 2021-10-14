63a393b4b7fad4c69dd3e29bd55f527a41209a0d
pavani-weerakoon <66486141+pavani-weerakoon@users.noreply.github.com>
2021-10-15 01:25:39 +0530

    changed jobs models.py

--- jobs/models.py
+++ jobs/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class Company(models.Model):
-    company_name = models.CharField(
+    company = models.CharField(
         max_length=200,
     )
     location = models.CharField(
@@ -22,11 +22,11 @@ class Job(models.Model):
     )
     company = models.ForeignKey(
         Company,
-        on_delete=models.PROTECT,
+        on_delete=models.CASCADE,
     )
 
 
-class section(models.Models):
+class Section(models.Model):
     section_name = models.CharField(
         max_length=200,
     )
@@ -38,11 +38,26 @@ class Question(models.Model):
     )
     section = models.ForeignKey(
         Section,
-        on_delete=models.PROTECT,
+        on_delete=models.CASCADE,
+    )
+    job = models.ForeignKey(
+        Job,
+        on_delete=models.CASCADE,
     )
 
 
 class Candidate(models.Model):
+    job = models.ForeignKey(
+        Job,
+        on_delete=models.CASCADE,
+    )
+    company = models.ForeignKey(
+        Company,
+        on_delete=models.CASCADE,
+    )
+
+
+class User(models.Models):
     first_name = models.CharField(
         max_length=200,
     )
@@ -52,16 +67,50 @@ class Candidate(models.Model):
     email = models.EmailField(
         max_length=200,
         blank=True, null=True
-
     )
     contact_number = models.IntegerField(
         null=True
     )
-    job = models.ForeignKey(
-        Job,
+    company = models.ForeignKey(
+        Company,
         on_delete=models.CASCADE,
+    )
+
+
+class Interviewer(models.Model):
+    role = models.CharField(
+        max_length=200,
     )
     company = models.ForeignKey(
         Company,
         on_delete=models.CASCADE,
+    )
+
+
+class Interview(models.Models):
+    interviewer = models.ForeignKey(
+        Interviewer,
+        on_delete=models.CASCADE,
+    )
+
+
+class Experience(models.Models):
+    role = models.CharField(
+        max_length=200,
+    )
+    worked_place = models.CharField(
+        max_length=100,
+    )
+    time_period = models.CharField(
+        max_length=200,
+    )
+    Candidate = models.ForeignKey(
+        Candidate,
+        on_delete=models.CASCADE,
+    )
+
+
+class Admin(models.Model):
+    admin_role = models.CharField(
+        max_length=200,
     )
